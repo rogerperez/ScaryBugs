cf4c3e093e0b0c3b197288ab30b2990bff6ca9e9
rogerperez <rogerperez@u.boisestate.edu>
2014-03-19 15:05:10 -0600

    + 5 start rating functionality / images

---
+++ ScaryBugs/RPGRateView.h
@@ -0,0 +1,31 @@
+//
+//  RateView.h
+//  CustomView
+//
+//  Created by Ray Wenderlich on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RPGRateView;
+
+@protocol RPGRateViewDelegate
+- (void)rateView:(RPGRateView *)rateView ratingDidChange:(float)rating;
+@end
+
+@interface RPGRateView : UIView
+
+@property (strong, nonatomic) UIImage *notSelectedImage;
+@property (strong, nonatomic) UIImage *halfSelectedImage;
+@property (strong, nonatomic) UIImage *fullSelectedImage;
+@property (assign, nonatomic) float rating;
+@property (assign) BOOL editable;
+@property (strong) NSMutableArray * imageViews;
+@property (assign, nonatomic) int maxRating;
+@property (assign) int midMargin;
+@property (assign) int leftMargin;
+@property (assign) CGSize minImageSize;
+@property (assign) id <RPGRateViewDelegate> delegate;
+
+@end
